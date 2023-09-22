a4c53317fc87d0cc0dd110d528ad0ed9f6d5a339
jhanva <jarbey.mejia@gmail.com>
2023-09-22 16:09:49 -0500

    feat: archivos de configuracion

--- python/metadata/path.py
+++ python/metadata/path.py
@@ -3,7 +3,14 @@ import os
 
 class Path:
     """Paths required for project execution."""
+    path_file = os.path.dirname(os.path.abspath(__file__))
 
     data = os.path.join('..', '..', 'data')
 
+    config = os.path.join('input', 'config.yaml')
+
+    kaggle_config = os.path.join('input', 'kaggle.json')
+
     qnn_model_example = os.path.join('..', '..', 'output', 'model_train.pt')
+
+    output = os.path.join('output')
