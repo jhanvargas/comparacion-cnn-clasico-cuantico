d82e5db644e6358bfad361ba505a09cfed7610ef
jhanva <jarbey.mejia@gmail.com>
2023-09-28 23:44:17 -0500

    fix: actualiza run

--- run.py
+++ run.py
@@ -1,16 +1,11 @@
 # Own libraries
 from python.feature_engineering import feature_pipeline
-from python.feature_engineering.utils.data_clean import show_random_image
-from python.metadata.path import Path
-from python.models import classic_models
-from python.ibm_quantum.utils.connect import get_ibm_quantum
+from python.models import neural_networks
 
 
 if __name__ == '__main__':
     feature_pipeline.executor()
     """Pipeline de feature engineering."""
 
-    show_random_image(Path.portrait)
-
-    classic_models.executor()
+    neural_networks.executor()
     """Pipeline de modelos."""
