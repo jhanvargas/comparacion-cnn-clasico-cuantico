19d0ed4d1f023186817370f0b27f2edb1bd0bb4b
jhanva <jarbey.mejia@gmail.com>
2023-09-28 23:33:48 -0500

    fix: actualiza nombre de modulos

--- python/models/neural_networks.py
+++ python/models/neural_networks.py
@@ -1,8 +1,12 @@
 # Own libraries
 from python.models.cnn.tensorflow import tensorflow_model
 from python.models.cnn.torch import torch_model
+from python.models.cnn.hybrid import hybrid_model
 
 
 def executor():
     tensorflow_model()
+
     torch_model()
+
+    hybrid_model()
