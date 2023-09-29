f011dd0922357fcf780e53b21fc5adf0eaab8567
jhanva <jarbey.mejia@gmail.com>
2023-09-28 23:45:56 -0500

    fix: actualiza script de pipeline de modelos

--- python/models/neural_networks.py
+++ python/models/neural_networks.py
@@ -5,8 +5,13 @@ from python.models.cnn.hybrid import hybrid_model
 
 
 def executor():
+    """Pipelines de los modelos de clasificación de imágenes."""
+
     tensorflow_model()
+    """Modelo de CNN usando tensowflow."""
 
     torch_model()
+    """Modelo de CNN usando PyTorch."""
 
     hybrid_model()
+    """Modelo de CNN usando PyTorch y Qiskit."""
