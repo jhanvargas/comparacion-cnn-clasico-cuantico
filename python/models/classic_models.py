8eae7d9422f8d88af1e92ed2bf9fc935276369ba
jhanva <jarbey.mejia@gmail.com>
2023-09-28 19:30:49 -0500

    feat: pipelines de modelos cnn

--- python/models/classic_models.py
+++ python/models/classic_models.py
@@ -1,8 +1,8 @@
-# External libraries
-
 # Own libraries
 from python.models.cnn.tensorflow import tensorflow_model
+from python.models.cnn.torch import torch_model
 
 
 def executor():
     tensorflow_model()
+    torch_model()
--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from python.feature_engineering import feature_pipeline
 from python.feature_engineering.utils.data_clean import show_random_image
 from python.metadata.path import Path
 from python.models import classic_models
+from python.ibm_quantum.utils.connect import get_ibm_quantum
+
 
 if __name__ == '__main__':
     feature_pipeline.executor()
